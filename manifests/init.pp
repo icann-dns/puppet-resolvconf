9a7145dbd81a37340da8eeae82b5fe582171f0bd
b4ldr <github@johnbond.org>
2015-06-25 23:44:25 +0100

    update skeleton

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,38 @@
-# == Class: resolvconf
+# == Class: network
 #
-# Full description of class resolvconf here.
+# Configure Network
 #
 # === Parameters
 #
 # Document parameters here.
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
 # === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
 #
 # === Examples
 #
-#  class { 'resolvconf':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  class { network:
+#      interfaces = hash of interfaces
+#      service4 = Service address used for dummy interfaces
+#      service6 = Service address used for dummy interfaces
+#      beacon4 = Service address used for dummy interfaces
+#      beacon6 = Service address used for dummy interfaces
 #  }
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# John Bond <john.bond@icann.org>
 #
 # === Copyright
 #
-# Copyright 2015 Your name here, unless otherwise noted.
+# Copyright 2014 Your name here, unless otherwise noted.
 #
 class resolvconf {
-
-
+  file {'/etc/resolvconf.conf':
+    ensure  => present,
+    content => template('resolvconf/etc/resolvconf.conf.erb'),
+  }
+  exec { '/sbin/resolvconf -u':
+    refreshonly => true,
+    subscribe   => File['/etc/resolvconf.conf'],
+  }
 }
